3661dfd142804a3bba2cb9b4eace523dd20e0958
mi4u <michal.lipinski@gmail.com>
2025-02-19 00:05:52 +0000

    docs

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+cp ../README.md gen-docs01/docs/docs/readme.md
+cp ../libraries/README.md gen-docs01/docs/docs/libs.md
+cp ../programs/README.md gen-docs01/docs/docs/programs.md
+
 cd gen-docs01/docs
 uv run mkdocs build -d ../../site
 cd ../..
